611c614b26c123ddd7f88f0444141d760703240b
Tinashe Chihoro <tinashechihoro@gmail.com>
2021-03-13 12:00:04 +0200

    category fueld added on the product model

--- product/models.py
+++ product/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 
 
 class Product(models.Model):
@@ -17,6 +16,7 @@ class Product(models.Model):
     description = models.TextField(max_length=500)
     condition = models.CharField(max_length=200, choices=CONDITION_CHOICES)
     price = models.DecimalField(max_digits=10, decimal_places=3)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
