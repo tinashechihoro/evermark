02e8d0fc7f98d55c3556e354d09a8b262880d0b2
Tinashe Chihoro <tinashechihoro@gmail.com>
2021-03-13 11:47:06 +0200

    product category class added

--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -18,6 +19,17 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=3)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    """
+    Product Category
+    """
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
