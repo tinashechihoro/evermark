14454cca839bd93d7ff37436f7de47b0d628ab6d
Tinashe Chihoro <tinashechihoro@gmail.com>
2021-03-15 18:02:03 +0200

    products modela nd views added

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ from django.urls import path , include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('products', include('product.urls')),
+                  path('', include('product.urls')),
               ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- product/urls.py
+++ product/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
 
     path('', views.product_list, name='product_list'),
-    path('<int:id>', views.product_detail, name='product_detail'),
+    path('<slug:product_slug>', views.product_detail, name='product_detail'),
 ]
