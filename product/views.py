2bee4c00fd4652377848b28b1f9f9d0712897693
Tinashe Chihoro <tinashechihoro@gmail.com>
2021-03-17 14:10:51 +0200

    added the categories count on the product list view

--- product/views.py
+++ product/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.db.models import Count
 from .models import Product, ProductCategory, ProductImages
 
 
 def product_list(request):
+    categories = ProductCategory.objects.annotate(total_products=Count('product'))
+
     products = Product.objects.all()
+    paginator = Paginator(products, 10)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+
     product_category = ProductCategory.objects.all
     context = {
         'products': products,
-        'product_category':product_category
+        'product_category': product_category,
+        'categories': categories
 
     }
     return render(request, 'product/product_list.html', context)
@@ -18,7 +27,7 @@ def product_detail(request, product_slug):
     product_images = ProductImages.objects.filter(product=product)
     context = {
         'product': product,
-        'product_images':product_images
+        'product_images': product_images
 
     }
     return render(request, 'product/product_detail.html', context)
