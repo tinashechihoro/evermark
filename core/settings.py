28ae969d8a55730f4367c5272a6775b42d39119f
Tinashe Chihoro <tinashechihoro@gmail.com>
2021-03-24 13:22:14 +0200

    hopefully the static error is sorted after adding the static_root

--- core/settings.py
+++ core/settings.py
@@ -106,8 +106,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+STATIC_URL = '/static/'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
